fb561263cd4f8c208ffff76274c38d1b955af7d2
Administrator <js1081996@gmail.com>
2025-03-16 23:51:10 +0530

    Struct Vs Classes

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -7,83 +7,72 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp2
-{
+/* 
+ * Structs 
+ * Just like classes, structs can have 
+ * private fields, public properties, constructors, methods, etc.
+ */
 
-    public class Student {
+namespace CSharpConcepts
+{
+    public struct Customer
+    {
         private int _id;
         private string _name;
-        private int _passMarks = 35;
-        public string city { set; get; }
-        public string email { set; get; }
 
-
+        // set and get accessor    
         public int Id
         {
-            set
-            {
-
-                if (value <= 0)
-                {
-                    throw new ArgumentException("student Id can not be null");
-                }
-                this._id = value;
-            }
-            get
-            {
-                return this._id;
-            }
+            get { return _id; }
+            set { _id = value; }
         }
-
-       
-
         public string Name
         {
-            set
-            {
-                if (string.IsNullOrEmpty(value))
-                {
-                    throw new ArgumentException("Name can not be null");
-                }
-                this._name = value;
-            }
-            get
-            {
-                return string.IsNullOrEmpty(this._name) ? "No Name" : this._name;
-            }
+            get { return _name; }
+            set { _name = value; }
         }
-       
-       public void SetPassMakrs(int PassMakrs)
+        // constructor
+        public Customer(int Id, string Name)
         {
-            this._passMarks = PassMakrs;
+            _id = Id;
+            _name = Name;
         }
-        public int passMakrs
+        //Method
+        public void PrintDetails()
         {
-            get
-            {
-                return (int)this._passMarks;
-            }
+            Console.WriteLine("Id = {0} && Name = {1}", _id, _name);
         }
-            
     }
-      
+
     class Program
     {
         static void Main(string[] args)
         {
-             
-            Student student = new Student();
-            student.Id =22;
-            student.Name ="Kira";
-            student.city = "Bangalore";
-            student.email = "Kira@gmail.com";
-            Console.WriteLine("Student Id ={0}",student.Id);
-            Console.WriteLine("Student Name ={0}",student.Name);
-            Console.WriteLine("studen PassMarks ={0}", student.passMakrs);
-            Console.WriteLine("studen City ={0}", student.city);
-            Console.WriteLine("studen Email ={0}", student.email);
-
+              Customer c1 = new Customer(101, "Mark");
+            c1.PrintDetails();
+            Customer c2 = new Customer();
+            c2.Id = 102;  
+            c2.Name = "John";
+            c2.PrintDetails();
+            Customer c3 = new Customer
+            {
+                Id = 103,
+                Name = "Rob"
+            };
+            c3.PrintDetails();
 
         }
+
     }
 }
+/*  
+ *  Difference between class and struct
+ *  struct is value type and class is reference type.
+ *  struct are stored on stack and class are stored on heap.
+ *  when you copy a struct to another struct, a new copy of the struct will be created.
+ *  But when you copy a class to another class, both will point to the same memory location.
+  *  struct can't have destructors but class can have destructors.
+  *  struct can't have explicit parameterless constructor but class can have.
+  *  struct can't inherit from another struct but class can inherit from another class.
+  *  A class can inherit from a struct.struct are sealed type, so they can't be inherited. 
+  */
